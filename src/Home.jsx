76c623c586958d5d2da8f0b15ab71685e850b04e
dev-nomi <noman.khalid.freelance@gmail.com>
2022-08-14 07:53:14 +0500

    Refactor the code and added blogs component.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Blogs from './components/Blogs'
 
 const Home =  () => {
   const [blogs, setBlogs] = useState([
@@ -9,12 +10,7 @@ const Home =  () => {
 
   return(
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>written by { blog.author }</p>
-        </div>
-      ))}    
+      <Blogs blogs={blogs} />
     </div>
   );
 }
